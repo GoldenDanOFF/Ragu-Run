4bc8fa93cb8ac5f36329d36f0d3daf75bc3cb45d
GoldenDanOFF <GOLDENDANBUSINESS@GMAIL.COM>
2024-04-06 03:38:12 +0300

    Build 0.1

---
+++ Assets/_Complete-Game/Scripts/kamera.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KameraScript : MonoBehaviour
+{
+    private Transform player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void LateUpdate()
+    {
+        Vector3 temp = transform.position;
+        temp.x = player.position.x;
+        temp.y = player.position.y;
+        transform.position = temp;
+    }
+}
+
